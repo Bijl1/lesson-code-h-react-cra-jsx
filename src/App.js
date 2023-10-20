9b7acf380dd23572b90202df76d60a293251f768
Jonathan Bijl <sannoayo@gmail.com>
2023-10-20 18:50:00 -0400

    Rendering the React App

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-
-      <header className="App-header">
-      <h1>Jonathan Bijl</h1>
-      <h2>Hello React!</h2>
-      <img src="https://i.imgur.com/8uPMJOs.png" className="App-logo" alt="logo" />
-    </header>
-      
+      <Header />
+    
     </div>
   );
 }
