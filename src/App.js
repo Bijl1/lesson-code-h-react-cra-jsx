7dbf4cafe48c583660987555221b80e3b1fb53c8
Jonathan Bijl <sannoayo@gmail.com>
2023-10-20 18:26:40 -0400

    Create React App

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <header className="App-header">
+      <h1>Jonathan Bijl</h1>
+      <h2>Hello React!</h2>
+      <img src="https://i.imgur.com/8uPMJOs.png" className="App-logo" alt="logo" />
+    </header>
+      
+    </div>
+  );
+}
+
+export default App;
