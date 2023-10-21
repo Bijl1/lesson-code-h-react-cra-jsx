bf35526fe7df990f36fd1fb0d2fe8d6f6453b0cf
Jonathan Bijl <sannoayo@gmail.com>
2023-10-20 20:20:35 -0400

    Embed function execution

--- src/App.js
+++ src/App.js
@@ -9,14 +9,39 @@ const quote = {
   secondQuote: 'Programmng isnt about what you know, it is about what you can figure out.'
 };
 
+const student = {
+  firstName: 'sterling',
+  lastName: 'bijl'
+}
+
+const { firstName, lastName } = student;
+
+function capitalizeFirstLetter(str) {
+  return str[0].toUpperCase() + str.slice(1);
+}
+
 function App() {
   return (
     <div className="App">
       {element}
       {heading}
       <h3>
-        Quote of the day, {quote.firstQuote} {quote.secondQuote}!
+        Quote of the day, {quote.firstQuote}!
       </h3>
+      <h4>
+        Hi, {student.firstName} {student.lastName}!
+      </h4>
+
+      <h4>
+        In uppercase: {firstName.toUpperCase()} {lastName.toUpperCase()}.
+      </h4>
+
+      <h4>{quote.secondQuote.toLocaleUpperCase()}</h4>
+
+      <h4>
+        Capitalized first letter: { capitalizeFirstLetter(firstName)} { capitalizeFirstLetter(lastName)}
+      </h4>
+
       <Header />
     
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Header() {
   return (
     <header className="App-header">
-        <h2>Jonathan Bijl</h2>
+        <h2>Header Page</h2>
         <img src="https://i.imgur.com/8uPMJOs.png" className="App-logo" alt="logo" />
     </header>
   );
