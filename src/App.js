548a8885eaeee1a4e81308077153381f9a6f6aa2
Jonathan Bijl <sannoayo@gmail.com>
2023-10-21 20:02:49 -0400

    Embed values in JSX

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import droneLogo from './images/drone.png';
 const element = <h1>🖖Hello world!</h1>;
 //Adding comments
 const heading = <h1> React is cool!</h1>;
+const name = <h1>Jonathan</h1>
 
 const quote = {
   firstQuote: 'The best error message is the one that never shows up.',
   secondQuote: 'Programmng isnt about what you know, it is about what you can figure out.'
 };
+
 
 const student = {
   firstName: 'sterling',
@@ -25,14 +27,24 @@ function capitalizeFirstLetter(str) {
 const theId = 'home';
 const divElement = <div id={theId}>div with an id</div>
 
+const drone = {
+  droneOne: 'nazgul',
+  droneTwo: 'flywoo baby',
+  droneThree: 'cinelog'
+}
+
 function App() {
   return (
     <div className="App">
-      
+      {name}
       {element}
       {divElement}
       
-      <img src={droneLogo} alt="Drone image" />
+      <img src={droneLogo} alt="Drone view" />
+      <img src='https://media.istockphoto.com/id/1412049078/photo/regatta-sailing-ship-yachts-with-white-sails-at-opened-sea-aerial-view-of-sailboat-in-windy.webp?b=1&s=170667a&w=0&k=20&c=n1bfQSuiJT_d1Q_dg6UnPmHcobpsy7W7PGTV-0Kc89k=' alt="sailing away"/>
+      <h4>{ capitalizeFirstLetter(drone.droneOne)}</h4>
+      <h1> {drone.droneTwo} {drone.droneThree}</h1>
+
       {heading}
       <h3>
         Quote of the day, {quote.firstQuote}!
